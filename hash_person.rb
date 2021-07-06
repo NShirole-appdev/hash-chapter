2c73c291090b7a5aa607f563a66b8afea0c72ddd
Nina Shirole <ninashirole@uchicago.edu>
2021-07-06 04:27:02 +0000

    hash person

--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,19 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
+
+#"Enter a name, age, and occupation separated by spaces:"
+#Maude 24 Artist
+#{ :name => "Maude", :age => 24, :occupation => "Artist" }
+
 p "Enter a name, age, and occupation separated by spaces:"
+personinput = gets.chomp.split()
+
+
+person1 = Hash.new
+
+person1.store(:name, personinput[0])
+
+person1.store(:age, personinput[1].to_i)
+
+person1.store(:occupation, personinput[2])
+
+puts person1
